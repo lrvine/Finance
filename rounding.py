c84fd63a47d2d538cc08e11807bc0541652e8cc7
lrvine <tsungyeh@gmail.com>
2018-07-23 22:16:10 +0800

    use auto format

--- rounding.py
+++ rounding.py
@@ -1,20 +1,23 @@
 import sys
 
+
 def round_to_base(x, base=5):
     return int(base * round(float(x)/base))
 
-def round_to_nth_digits_after_decimal_point_with_base_m(x,n=5,m=5):
+
+def round_to_nth_digits_after_decimal_point_with_base_m(x, n=5, m=5):
     x = x*(10**(n+1))
     x = round_to_base(x, 10)
     x = x/10
-    x = round_to_base(x , m)
+    x = round_to_base(x, m)
     x = x/float(10**n)
     return x
 
-if __name__ == "__main__":
-	try:
-		print round_to_nth_digits_after_decimal_point_with_base_m(float(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
-		print '\n'
-    	except KeyboardInterrupt:
-        	print "\n\nUser Press Ctrl+C,Exit"
 
+if __name__ == "__main__":
+    try:
+        print round_to_nth_digits_after_decimal_point_with_base_m(
+            float(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+        print '\n'
+    except KeyboardInterrupt:
+        print "\n\nUser Press Ctrl+C,Exit"
--- stock.py
+++ stock.py
@@ -4,43 +4,45 @@ import sys
 
 
 def get_quote(symbol):
-	base_url = 'http://www.bloomberg.com/quote/'
-	content = urllib.urlopen(base_url + symbol +':TT' ).read()
-	m = re.search('<div class="price">(.*?)</div>', content)
-	if m:
-		quote = m.group(1)
-	else:
-		quote = 'no quote available for: ' + symbol
-	return quote
+    base_url = 'http://www.bloomberg.com/quote/'
+    content = urllib.urlopen(base_url + symbol + ':TT').read()
+    m = re.search('<div class="price">(.*?)</div>', content)
+    if m:
+        quote = m.group(1)
+    else:
+        quote = 'no quote available for: ' + symbol
+    return quote
+
 
 def get_estimate(symbol):
-	base_url = 'https://www.google.com/finance?q=TPE:'
-	content = urllib.urlopen(base_url + symbol).read()
-	m = re.findall('<td class="val">(.*?)\s', content)
-	dif=0
-	if m:
-		print ' P/E ratio is ',m[5]
-		print ' estimate EPS is ',m[7]
-	      	r=float(m[7])*20;
-		print 'current reasonable price for 5% retrun might be ',r
-		r=float(m[7])*25;
-		print 'current reasonable price for 4% retrun might be ',r
-		r=float(m[7])*50;
-		print 'current reasonable price for 2% retrun might be ',r
-	else:
-		print ' get estimate EPS failure ' + symbol
+    base_url = 'https://www.google.com/finance?q=TPE:'
+    content = urllib.urlopen(base_url + symbol).read()
+    m = re.findall('<td class="val">(.*?)\s', content)
+    dif = 0
+    if m:
+        print ' P/E ratio is ', m[5]
+        print ' estimate EPS is ', m[7]
+        r = float(m[7])*20
+        print 'current reasonable price for 5% retrun might be ', r
+        r = float(m[7])*25
+        print 'current reasonable price for 4% retrun might be ', r
+        r = float(m[7])*50
+        print 'current reasonable price for 2% retrun might be ', r
+    else:
+        print ' get estimate EPS failure ' + symbol
+
 
 def p_price(symbol):
-	print symbol+' current price is ',get_quote(symbol)
-	get_estimate(symbol);
+    print symbol+' current price is ', get_quote(symbol)
+    get_estimate(symbol)
 
 
 if __name__ == "__main__":
-	try:
-		stocklist = open(sys.argv[1]).read().splitlines()
-		print '\n'
-		for line in stocklist:
-			p_price(line)
-			print '\n'
-    	except KeyboardInterrupt:
-        	print "\n\nUser Press Ctrl+C,Exit"
+    try:
+        stocklist = open(sys.argv[1]).read().splitlines()
+        print '\n'
+        for line in stocklist:
+            p_price(line)
+            print '\n'
+    except KeyboardInterrupt:
+        print "\n\nUser Press Ctrl+C,Exit"
